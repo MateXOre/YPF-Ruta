965195b6532ec0d33454ae82a2a981673090c81d
GianLucaSpagnolo <gianluuba@gmail.com>
2025-11-25 00:11:33 -0300

    pruebas implementadas para ypf_ruta y estacion

--- ypf_ruta/src/actores/mod.rs
+++ ypf_ruta/src/actores/mod.rs
@@ -2,4 +2,4 @@ pub mod empresa;
 pub mod estacion;
 pub mod gestor;
 pub mod peer;
-pub(crate) mod ypf;
+pub mod ypf;
--- ypf_ruta/src/actores/ypf/mod.rs
+++ ypf_ruta/src/actores/ypf/mod.rs
@@ -1,3 +1,6 @@
+#![allow(unused_imports)]
 mod handlers;
 pub mod messages;
 pub mod ypf_actor;
+
+pub use ypf_actor::YpfRuta;
